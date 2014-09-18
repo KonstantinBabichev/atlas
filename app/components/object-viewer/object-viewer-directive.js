79c261b94477cfe837e4b9c2b7ad50424f5e4088
Scott Nath <github@scottnath.com>
2014-09-18 14:16:25 -0400

    object viewer component

--- app/components/object-viewer/object-viewer-directive.js
+++ app/components/object-viewer/object-viewer-directive.js
@@ -1,10 +1,20 @@
 angular.module('angularApp')
-	.directive('objectViewer', function() {
+	.directive('objectViewer', function(RecursionHelper) {
 		return {
 			restrict: 'E',
 			templateUrl: 'components/object-viewer/object-viewer.html',
+      scope: {object: '='},
+      compile: function(element) {
+        console.log(element);
+        return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){
+          // Define your normal link function here.
+          // Alternative: instead of passing a function,
+          // you can also pass an object with
+          // a 'pre'- and 'post'-link function.
+        });
+      },
 			link: function(scope, element, attrs, fn) {
-
+        //var tester = "a test again";
 
 			}
 		};
