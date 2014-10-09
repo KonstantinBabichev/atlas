6eccf7cd24fcdf128076c2bfdcfde5bc51c20705
Scott Nath <github@scottnath.com>
2014-10-09 14:14:17 -0400

    object viewer replaced by bower-based version

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('angularApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ngRoute','templatescache', 'objectViewer'])
+angular.module('angularApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ngRoute','templatescache', 'angularObjectViewer'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
